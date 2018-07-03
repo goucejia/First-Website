ab53e4a693714d2ea46ac27cfae66ee23428d391
goucejia <32345479+goucejia@users.noreply.github.com>
2018-07-03 18:15:15 +0800

    fixed navbar issue, adding some minor functionality (addAlbum not working)

--- music/urls.py
+++ music/urls.py
@@ -6,15 +6,18 @@ app_name = "music"
 
 urlpatterns = [
     # http://127.0.0.1:8000/music/
-    url(r'^$', views.index, name="index"), # based home page
+    url(r'^$', views.IndexView.as_view(), name="index"), # based home page
+    # url(r'^$', views.index, name="index"), # based home page
 
-    # http://127.0.0.1:8000/music/<album_id>
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "display_album"),
+    # http://127.0.0.1:8000/music/<album_id> 
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "display_album"),
+    # url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "display_album"),
 
     # http://127.0.0.1:8000/music/<album_id>/favorite
     url(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name = "favorite"),
 
-    # # http://127.0.0.1:8000/music/<album_id>/unfavorite
-    # url(r'^(?P<album_id>[0-9]+)/unfavorite$', views.unfavorite, name = "unfavorite"),
+    # music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view, name = 'album_add'),
+
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,31 +1,47 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
 from django.template import loader
 from django.http import HttpResponse
 from .models import Album, Song
 from django.http import Http404
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import View
+from .forms import UserForm
 
 # Create your views here.
 
-def index(request) :
-    all_album = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        "all_album" : all_album,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_album'
 
-    # return render(request, 'music/index.html', context)
+    def get_queryset(self):
+        return Album.objects.all()
 
-def detail(request, album_id) :
-    # return HttpResponse("<h2> Details for album " + str(album_id) + " </h2>")
+# def index(request) :
+#     all_album = Album.objects.all()
+#     template = loader.get_template('music/index.html')
+#     context = {
+#         "all_album" : all_album,
+#     }
+#     return HttpResponse(template.render(context, request))
+#     # return render(request, 'music/index.html', context)
 
-    # try:
-    #     album = Album.objects.get(pk = album_id)
-    # except Album.DoesNotExist:
-    #     raise Http404("Album Does Not Exist")
 
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album' : album})
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+# def detail(request, album_id) :
+#     # try:
+#     #     album = Album.objects.get(pk = album_id)
+#     # except Album.DoesNotExist:
+#     #     raise Http404("Album Does Not Exist")
+#
+#     album = get_object_or_404(Album, pk=album_id)
+#     return render(request, 'music/detail.html', {'album' : album})
+
 
 
 def favorite(request, album_id):
@@ -38,25 +54,11 @@ def favorite(request, album_id):
             "error_msg" : "Song not valid",
         }
     else:
-        # if selected_song:
-        #     selected_song.is_favorite = False
-        #     selected_song.save()
-        # else:
         selected_song.is_favorite = True
         selected_song.save()
         return render(request, 'music/detail.html', {'album': album})
 
-# def unfavorite(request, album_id) :
-#     album = get_object_or_404(Album, pk = album_id)
-#     album = get_object_or_404(Album, pk=album_id)
-#     try:
-#         selected_song = album.song_set.get(pk=request.POST["song"])
-#     except(KeyError, Song.DoesNotExist):
-#         return (request, 'music/detail.html'), {
-#             'album': album,
-#             "error_msg": "Song not valid",
-#         }
-#     else:
-#         selected_song.is_favorite = False
-#         selected_song.save()
-#         return render(request, 'music/detail.html', {'album': album})
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'title', 'genre', 'album_logo']
